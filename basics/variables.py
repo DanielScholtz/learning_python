e4e44d0dd487b8a3c7e4b63346a431698bf990b9
daniel_scholtz <daniel_scholtz@epam.com>
2019-09-13 21:51:23 +0200

    Initial commit

---
+++ basics/variables.py
@@ -0,0 +1,26 @@
+my_int = 7
+print(my_int)
+
+my_float = 7.0
+print(my_float)
+my_float = float(7)
+print(my_float)
+
+my_string = 'hello'
+print(my_string)
+my_string = "hello"
+print(my_string)
+
+my_string_with_apostrophes = "Don't worry about apostrophes"
+print(my_string_with_apostrophes)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+hello_world = hello + " " + world
+print(hello_world)
+
